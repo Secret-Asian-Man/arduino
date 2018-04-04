9616dbc031f4246c32917c9fe7178f1f570ecbce
root <root@DESKTOP-494IM56.localdomain>
2018-04-03 21:15:02 -0700

    Make pulse function more generic

--- WaterPulseFountain/WaterPulseFountain.ino
+++ WaterPulseFountain/WaterPulseFountain.ino
@@ -1,13 +1,15 @@
-//#define MOSFET_PIN 4
+#define PUMP_PIN 4 // For pump
 #define POT_SIGOUT_PIN 9
 #define POT_SIGIN_PIN A0
 
 #define BLINK_DELAY 50
+#define PUMP_DELAY 50
+#define PUMP_INTERVAL 100 // TODO: Needs testing
 int pot_map = 0;
 
 void setup() {                
   Serial.begin(9600);
-  //pinMode(MOSFET_PIN, OUTPUT);
+  pinMode(PUMP_PIN, OUTPUT);
   pinMode(POT_SIGIN_PIN, INPUT);
   pinMode(POT_SIGOUT_PIN, OUTPUT);
 }
@@ -15,12 +17,13 @@ void setup() {
 void loop() {
   pot_map = map(analogRead(POT_SIGIN_PIN), 0, 1023, 0, 255);
   Serial.println(pot_map);
-  strobeLight(POT_SIGOUT_PIN, pot_map);
+  pulse(POT_SIGOUT_PIN, BLINK_DELAY,pot_map);
+  pulse(PUMP_PIN, PUMP_DELAY, PUMP_INTERVAL);
 }
 
-void strobeLight(char pin, int interval){
+void pulse(char pin, int pulseDelay int interval){
   digitalWrite(pin,1);
-  delay(BLINK_DELAY);
+  delay(pulseDelay);
   digitalWrite(pin,0);
   delay(interval);
   return;
