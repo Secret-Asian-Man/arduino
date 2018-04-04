26c152e2ddcc0c0ad950e21dcc3801bc50f2324f
root <root@DESKTOP-494IM56.localdomain>
2018-04-03 20:54:55 -0700

    Starting from scratch, using Arduino uno

--- WaterPulseFountain/WaterPulseFountain.ino
+++ WaterPulseFountain/WaterPulseFountain.ino
@@ -1,15 +1,28 @@
-//#define LED_PIN 2
-#define MOSFET_PIN 4 //4
-#define POT_SIG_PIN 1 //P2
+//#define MOSFET_PIN 4
+#define POT_SIGOUT_PIN 9
+#define POT_SIGIN_PIN A0
+
+#define BLINK_DELAY 50
+int pot_map = 0;
 
 void setup() {                
   Serial.begin(9600);
-  pinMode(MOSFET_PIN, OUTPUT);
-//  pinMode(LED_PIN, OUTPUT);
+  //pinMode(MOSFET_PIN, OUTPUT);
+  pinMode(POT_SIGIN_PIN, INPUT);
+  pinMode(POT_SIGOUT_PIN, OUTPUT);
 }
 
-
-void loop() { 
-//  analogWrite(LED_PIN, 255/3);
-  analogWrite(MOSFET_PIN,map(analogRead(POT_SIG_PIN), 0, 1023, 0, 255));
+void loop() {
+  pot_map = map(analogRead(POT_SIGIN_PIN), 0, 1023, 0, 255);
+  Serial.println(pot_map);
+  strobeLight(POT_SIGOUT_PIN, pot_map);
 }
+
+void strobeLight(char pin, int interval){
+  digitalWrite(pin,1);
+  delay(BLINK_DELAY);
+  digitalWrite(pin,0);
+  delay(interval);
+  return;
+}
+
